ebbabb47b38e38ae9198492b8773e89cde4bcc14
Joseph Keogh <jgk7uf@virginia.edu>
2020-10-12 16:03:41 -0400

    basics finished

--- db_connection.R
+++ db_connection.R
@@ -1,11 +1,15 @@
 # Retrieve today's observations from the gen_by_fuel series
 library(data.table); library(httr); library(lubridate)
 
-r <- GET("https://api.pjm.com/api/v1/gen_by_fuel", add_headers("Ocp-Apim-Subscription-Key" = "625845c6fabc4639ab91428486d8d2e2"),
-         query = list(rowCount = "40000", startRow = "1", datetime_beginning_ept = 'Today'))
+pnode=34964545
+
+r <- GET("https://api.pjm.com/api/v1/rt_unverified_fivemin_lmps", add_headers("Ocp-Apim-Subscription-Key" = "625845c6fabc4639ab91428486d8d2e2"),
+         query = list(rowCount = 3000, startRow = "1", datetime_beginning_ept = 'CurrentWeek', pnode_id= pnode, sort='datetime_beginning_ept'))
 
 temp = httr::content(r,"parsed")
 
 gen_data_today = data.table::rbindlist(temp[['items']])
 
 print(head(gen_data_today))
+
+# Connect to the database
